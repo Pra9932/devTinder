f09b98fc436bb276087749a5568444888c741035
Pra9932 <1999prahallad@gmail.com>
2025-07-30 20:37:23 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -1,18 +1,58 @@
 
 const express=require("express");
 
+   const connectDB=require("./config/database");
+
 const app=express();
 
-app.use("/",(req,res)=>{
-    res.send("Hello from the dashboard");
-});
-app.use("/hello",(req,res)=>{
-    res.send("Hello hello hello !!!");
-});
+const User=require("./models/user");
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from the server!");
-});
-app.listen(7777,()=>{
+app.post("/signup",async(req,res)=>{
+
+    const user=new User({
+        firstName:"Prahallad",
+        lastName:"Das",
+        emailId:"dasprahallad2021@gmail.com",
+        password:"das@1234",
+    });
+
+    try{
+        await user.save();
+        res.send("User added Successfully");
+}catch(err){
+    res.status(400).send("Error saving the User:",err.message);
+}
+    
+    //creating new instance of User model
+    // const user=new User(userObj);
+})
+
+
+
+connectDB().then(()=>{
+    console.log("database Connection successfully....");
+    app.listen(7777,()=>{
     console.log("Server is successfully listening on port 7777:..");
 });
+
+}).catch(err=>{
+        console.error("database Can not be connected....");
+});
+//This will handle GET call to \user
+
+// app.get("/user",(req,res)=>{
+//     res.send({firstName:"Prahallad",lastName:"Das"});
+// });
+
+// // app.use("/hello",(req,res)=>{
+// //     res.send("Hello hello hello !!!");
+// // });
+
+// // app.use("/",(req,res)=>{
+// //     res.send("Hello from the dashboard");
+// // });
+
+
+// app.use("/test",(req,res)=>{
+//     res.send("Hello from the server!");
+// });
